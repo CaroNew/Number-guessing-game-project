49bbc4315504e84a3dba4ee43e2f8e431a19a7d1
Carolina Lucero <mateconpeperina23@gmail.com>
2024-08-11 15:04:24 +0000

    feat:add ramdom number

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Hello user!"
+# Number guessing game
+
+PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
+SECRET_NUMBER=$(( (RANDOM % 1000) + 1 ))
+ATTEMP=0
+
+echo "Test ramdom: $SECRET_NUMBER"
