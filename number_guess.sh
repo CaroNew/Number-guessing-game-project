caabf0c09899907662401a25487251e2ae2121d9
Carolina Lucero <mateconpeperina23@gmail.com>
2024-08-11 21:58:48 +0000

    feat: modifing main fuction

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,7 @@
 # Number guessing game
 
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
-SECRET_NUMBER=$(( (RANDOM % 10) + 1 ))
+SECRET_NUMBER=$(( (RANDOM % 1000) + 1 ))
 ATTEMP=0
 
 MAIN(){
